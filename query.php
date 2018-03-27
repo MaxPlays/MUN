3af200f145bdf51be25a3289f6e7ca21290e2695
MaxPlays <maxplays88@gmail.com>
2018-03-27 03:08:19 +0200

    Added license texts, removed debug-related content

--- query.php
+++ query.php
@@ -1,3 +1,18 @@
+<!--
+Copyright 2018 Maximilian Negedly
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-->
 <?php
 
   include "config.php";
@@ -11,11 +26,11 @@
       die("Connection failed: " . $conn->connect_error);
     }
 
-    $conn->query("CREATE TABLE IF NOT EXISTS wasamun(committee TEXT, topicA TEXT, topicB TEXT, data TEXT);");
+    $conn->query("CREATE TABLE IF NOT EXISTS mun(committee TEXT, topicA TEXT, topicB TEXT, data TEXT);");
 
     if(strcmp($action, "getCommittees") == 0){
       //echo '[{"name":"UNODC"}, {"name":"UNEP"}, {"name":"UNICEF"}]';
-      $result = $conn->query("SELECT committee FROM wasamun;");
+      $result = $conn->query("SELECT committee FROM mun;");
       $array = array();
       if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
@@ -26,7 +41,7 @@
     }else if(strcmp($action, "getInfo") == 0){
       if(isset($_GET["committee"]) && isset($_GET["topic"])){
         //echo '{"name": "Topic A: Hacking and crime in the dark web", "countries":[{"country":"Test 1","index":-1,"warns":0},{"country":"Test 2","index":-1,"warns":0},{"country":"Test 3","index":-1,"warns":0},{"country":"Test 4","index":-1,"warns":0},{"country":"Test 5","index":-1,"warns":0},{"country":"Test 6","index":-1,"warns":0}]}';
-        $stmt = $conn->prepare('SELECT * FROM wasamun WHERE committee=?;');
+        $stmt = $conn->prepare('SELECT * FROM mun WHERE committee=?;');
         $stmt->bind_param("s", $_GET["committee"]);
         $stmt->execute();
         $stmt->bind_result($committee, $topicA, $topicB, $data);
@@ -52,7 +67,7 @@
     }else if(strcmp($action, "save") == 0){
       if(isset($_GET["committee"]) && isset($_GET["data"])){
         $data = $_GET["data"];
-        $stmt = $conn->prepare('UPDATE wasamun SET data=? WHERE committee=?;');
+        $stmt = $conn->prepare('UPDATE mun SET data=? WHERE committee=?;');
         $stmt->bind_param("ss", $_GET["data"], $_GET["committee"]);
         $stmt->execute();
         $stmt->close();
@@ -69,14 +84,14 @@
         $topicB = $data->{"topicB"};
         $countries = json_encode($data->{"countries"});
 
-        $stmt = $conn->prepare('SELECT committee FROM wasamun WHERE committee=?;');
+        $stmt = $conn->prepare('SELECT committee FROM mun WHERE committee=?;');
         $stmt->bind_param("s", $committee);
         $stmt->execute();
         $stmt->bind_result($result);
         $stmt->fetch();
 
         if(strcmp($result, "") == 0){
-          $sstmt = $conn->prepare('INSERT INTO wasamun VALUES(?, ?, ?, ?);');
+          $sstmt = $conn->prepare('INSERT INTO mun VALUES(?, ?, ?, ?);');
           $sstmt->bind_param("ssss", $committee, $topicA, $topicB, $countries);
           $sstmt->execute();
           $sstmt->close();
@@ -92,7 +107,7 @@
       if(isset($_GET["name"])){
         $name = $_GET["name"];
 
-        $stmt = $conn->prepare('DELETE FROM wasamun WHERE committee=?;');
+        $stmt = $conn->prepare('DELETE FROM mun WHERE committee=?;');
         $stmt->bind_param("s", $name);
         $stmt->execute();
         $stmt->close();
@@ -102,7 +117,7 @@
         echo "Error";
       }
     }else if(strcmp($action, "getTableInfo") == 0){
-      $result = $conn->query("SELECT committee, topicA, topicB FROM wasamun;");
+      $result = $conn->query("SELECT committee, topicA, topicB FROM mun;");
       $array = array();
       if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
